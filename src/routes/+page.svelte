b4af81fe37c90075093eb27718aa9ca255490dac
Florian Klenk <florian.klenk@luciapp.de>
2025-02-14 15:54:32 +0100

    status

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import * as Accordion from '$lib/components/ui/accordion';
+</script>
+
+<Button variant="outline">Click me</Button>
+
+<Accordion.Root>
+	<Accordion.Item value="item-1">
+		<Accordion.Trigger>Is it accessible?</Accordion.Trigger>
+		<Accordion.Content>Yes. It adheres to the WAI-ARIA design pattern.</Accordion.Content>
+	</Accordion.Item>
+</Accordion.Root>
